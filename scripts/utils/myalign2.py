4f2b5f04fbfccf2b11f6be37cd519c76c4732d3e
sc8668 <91195970+sc8668@users.noreply.github.com>
2021-09-23 14:10:45 +0800

    Update myalign2.py

--- scripts/utils/myalign2.py
+++ scripts/utils/myalign2.py
@@ -59,25 +59,6 @@ def myoperation(t, pdb):
 	cmd += 'structconvert %s_complex_prep_ligand1.mae %s_l.mol2'%(pdb, pdb)
 	os.system(cmd)
 
-'''
-names = [x for x in os.listdir('./293') if os.path.isdir('./293/%s'%(x))]
-for name in names:
-	os.system("cp 332/%s/%s_prot/%s_complex_prep.mae 332/"%(name,name,name))
-
-total = ["293_%s_%s"%(x,y) for x in names for y in names if x!=y]
-my=[]
-x1=['1aid', '1b6j', '1b6k', '1b6l', '1d4k', '1d4l', '1mtr', '1z1h', '1kzk', '3aid', '3el1','3ekv', '3ekx','3mxd', '3mxe']
-for x in x1:
-	for y in x1:
-		if x!=y:
-			my.append("293_%s_%s"%(x,y))
-		
-final = [x for x in total if x not in my]
-
-dfx = df.loc[names]
-ww = [x for x in dfx.groupby('ligname') if len(x[-1])>1]
-'''
-
 
 
 
